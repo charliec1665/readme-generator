55bfe8d6b0add0a763c866fb16a20001115dffb1
Charlie Carter <charliec1665@gmail.com>
2022-09-09 14:42:40 -0400

    user input prompts

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project? (Required)'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project:'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    return
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(questions)
+};
 
 // Function call to initialize app
 init();
